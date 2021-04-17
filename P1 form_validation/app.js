3cf35391a42ca00e6a15892c0618e0487473ec80
Uzair Munir <uzair8825@gmail.com>
2021-04-18 00:51:30 +0500

    Update Project 01

---
+++ P1 form_validation/app.js
@@ -0,0 +1,60 @@
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmpassword');
+const form = document.getElementById('form');
+console.log(firstName);
+
+// Function to update class and message for errors
+const errorMessage = (input, message) => {
+  const formGroup = input.parentElement;
+  formGroup.className = 'form-group error';
+  const small = formGroup.querySelector('small');
+  small.innerText = message;
+};
+// Function To Show Success
+const showSuccess = (input) => {
+  const formGroup = input.parentElement;
+  formGroup.className = 'form-group success';
+};
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Validating First Name
+  if (firstName.value === '') {
+    errorMessage(firstName, 'This Field is Required');
+  } else {
+    showSuccess(firstName);
+  }
+  // Validating Last Name
+  if (lastName.value === '') {
+    errorMessage(lastName, 'This Field is Required');
+  } else {
+    showSuccess(lastName);
+  }
+  // Validating  Email
+  if (email.value === '') {
+    errorMessage(email, 'This Field is Required');
+  } else {
+    showSuccess(email);
+  }
+  // Validating Password
+  if (password.value === '') {
+    errorMessage(password, 'This Field is Required');
+  } else if (password.value.length <= 8) {
+    errorMessage(password, 'Password Must be Greater than 8');
+  } else {
+    showSuccess(password);
+  }
+  // Validating Confirm Password
+  if (confirmPassword.value === '') {
+    errorMessage(confirmPassword, 'This Field is Required');
+  } else if (confirmPassword.value !== password.value) {
+    errorMessage(
+      confirmPassword,
+      'Your password and confirmation password do not match. '
+    );
+  } else {
+    showSuccess(confirmPassword);
+  }
+});
