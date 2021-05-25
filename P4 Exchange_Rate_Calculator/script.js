744595ea5839409c33e7cdb4f0116d34656a4876
Uzair Munir <uzair8825@gmail.com>
2021-05-25 23:32:42 +0500

    added local storage functionality in project 04

--- P4 Exchange_Rate_Calculator/script.js
+++ P4 Exchange_Rate_Calculator/script.js
@@ -5,6 +5,8 @@ const currencyTwoAmount = document.getElementById('amount-two');
 const rate = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
+populateUi();
+
 const calculate = () => {
   const currencyOneCode = currencyOne.value;
   const currencyTwoCode = currencyTwo.value;
@@ -20,10 +22,29 @@ const calculate = () => {
       ).toFixed(2);
     });
 };
+// Set Items in Local Storage
+const setCurrencyOne = (currencyOne) => {
+  localStorage.setItem('setCurrencyOne', currencyOne);
+};
+const setCurrencyTwo = (currencyTwo) => {
+  localStorage.setItem('setCurrencyTwo', currencyTwo);
+};
+const setCurrencyOneAmount = (currencyOneAmount) => {
+  localStorage.setItem('setCurrencyOneAmount', currencyOneAmount);
+};
 // Event Listeners
-currencyOne.addEventListener('change', calculate);
-currencyOneAmount.addEventListener('input', calculate);
-currencyTwo.addEventListener('change', calculate);
+currencyOne.addEventListener('change', (e) => {
+  setCurrencyOne(e.target.value);
+  calculate();
+});
+currencyOneAmount.addEventListener('input', () => {
+  setCurrencyOneAmount(currencyOneAmount.value);
+  calculate();
+});
+currencyTwo.addEventListener('change', (e) => {
+  setCurrencyTwo(e.target.value);
+  calculate();
+});
 currencyTwoAmount.addEventListener('input', calculate);
 // Swap Currencies
 swap.addEventListener('click', () => {
@@ -32,4 +53,16 @@ swap.addEventListener('click', () => {
   currencyTwo.value = temp;
   calculate();
 });
+
+//Get data from Local Storage Update The Ui
+function populateUi() {
+  const selectedCurrencyOne = localStorage.getItem('setCurrencyOne');
+  const selectedCurrencyTwo = localStorage.getItem('setCurrencyTwo');
+  const setCurrencyOneAmount = localStorage.getItem('setCurrencyOneAmount');
+  if (selectedCurrencyOne && selectedCurrencyTwo && setCurrencyOneAmount) {
+    currencyOne.value = selectedCurrencyOne;
+    currencyTwo.value = selectedCurrencyTwo;
+    currencyOneAmount.value = setCurrencyOneAmount;
+  }
+}
 calculate();
